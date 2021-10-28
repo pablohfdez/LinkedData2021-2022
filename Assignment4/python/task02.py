683e41228bbf1b890e8ef12b2c5ef028a0888c01
ANDREA CIMMINO ARRIAGA <cimmino@us.es>
2021-10-28 17:13:37 +0200

    modified files

--- Assignment4/python/task02.py
+++ Assignment4/python/task02.py
@@ -12,7 +12,7 @@ Comenzamos con un grafo vacío
 """
 
 !pip install rdflib
-github_storage = "https://raw.githubusercontent.com/FacultadInformatica-LinkedData/Curso2020-2021/master/Assignment4"
+github_storage = "https://raw.githubusercontent.com/AndreaCimminoArriaga/LinkedData2021-2022/main/Assignment4/"
 
 from rdflib import Graph, Namespace, Literal
 g = Graph()
